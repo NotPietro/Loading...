25d231ed8c3c44d94009557aca1c4d57c4dc85e8
Pepe <128159900+NotPietro@users.noreply.github.com>
2023-10-04 12:06:42 -0300

    Add files via upload

---
+++ Loading/index.js
@@ -0,0 +1,17 @@
+const counterEl = document.querySelector(".counter");
+
+const barEl = document.querySelector(".Loading-bar-front");
+
+let idx = 0;
+
+updateNum();
+
+function updateNum(){
+    counterEl.innerHTML = idx + "%";
+    barEl.style.width = idx + "%"
+    idx ++;
+
+    if(idx < 101){
+        setTimeout(updateNum, 20);
+    }
+}
